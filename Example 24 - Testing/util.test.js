359e20d3478afc746fc50da50ee3148d76fed3b5
majasveljo <majasveljo@gmail.com>
2021-01-28 19:06:23 +0100

    testing module done

--- Example 24 - Testing/util.test.js
+++ Example 24 - Testing/util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
 test("shoud output name and age", () => {
@@ -21,3 +22,21 @@ test("should generate a valid text output", () => {
   const text = checkAndGenerate("Max", 29);
   expect(text).toBe("Max (29 years old)");
 });
+
+test("should create an element with text and correct class", async () => {
+  const browser = await puppeteer.launch({
+    // headless: false,
+    // slowMo: 80,
+    headless: true,
+    // args: ['--window-size=1920,1000']
+  })
+  const page = await browser.newPage();
+  await page.goto("file:///C:/Users/draga/Desktop/tutorijali/Maximilian%20JS/Example%2024%20-%20Testing/index.html")
+  await page.click('input#name')
+  await page.type('input#name', "Anna")
+  await page.click('input#age')
+  await page.type('input#age', "28")
+  await page.click('#btnAddUser')
+  const finalText = await page.$eval('.user-item', el => el.textContent)
+  expect(finalText).toBe("Anna (28 years old)")
+}, 10000)
